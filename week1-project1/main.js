07e0e0fa06b9081263f0c7159f54f8b45d815e32
mrtimpender <mrtimpender@gmail.com>
2016-01-18 22:55:10 -0700

    partial completion of assignment 1

--- week1-project1/main.js
+++ week1-project1/main.js
@@ -4,20 +4,44 @@
 // =======================================================
 
 //1. Log your name to the console (as a string).
+console.log("Tim Pender");
 
 //2. Declare a variable called name, log it to the console.
+var name = "Tim Pender";
+console.log(name);
 
 //3. Declare two variables with number values. Add, subtract, multiply and divide them.
+var num1 = 5
+var num2 = 1
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
 
 //4. Declare two variables with string values. Create a third variable that concatenates them.
+var name1 = "Tim";
+var name2 = "Pender";
+
+console.log(name1 + name2);
 
 //5. Add, subract, multiply and divide the strings '5' and '3'. Observe and explain the results.
 
 //6. Create a prompt that asks a user to pick a number. Double the answer.
+var timesTwo = prompt("Pick a number, any number.");
+console.log(2 * timesTwo);
 
 //7. Create a custom alert.
 
 //8. Declare three variables called length, width and height. Calculate area and volume.
+var length = 10
+var height = 15
+var width = 20
+var area = length * width;
+var volume = length * width * height;
+
+console.log(area);
+console.log(volume);
 
 //9. Ask for a user's name, age and city. In a single command display/log the sentence:
 // "My name is...I'm....years old, and I live in....."
