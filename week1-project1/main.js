8e3754f68058e1a6fa3324e9542afbbcf60eeb3d
mrtimpender <mrtimpender@gmail.com>
2016-01-20 18:09:23 -0700

    Lecture1 Assignment Complete

--- week1-project1/main.js
+++ week1-project1/main.js
@@ -3,47 +3,61 @@
 // Log all answers to the console and write them to document
 // =======================================================
 
-//1. Log your name to the console (as a string).
-console.log("Tim Pender");
+// //1. Log your name to the console (as a string).
+// console.log("Tim Pender");
 
-//2. Declare a variable called name, log it to the console.
-var name = "Tim Pender";
-console.log(name);
+// //2. Declare a variable called name, log it to the console.
+// var name = "Tim Pender";
+// console.log(name);
 
-//3. Declare two variables with number values. Add, subtract, multiply and divide them.
-var num1 = 5
-var num2 = 1
+// //3. Declare two variables with number values. Add, subtract, multiply and divide them.
+// var num1 = 5
+// var num2 = 1
 
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
 
-//4. Declare two variables with string values. Create a third variable that concatenates them.
-var name1 = "Tim";
-var name2 = "Pender";
+// //4. Declare two variables with string values. Create a third variable that concatenates them.
+// var name1 = "Tim ";
+// var name2 = "Pender";
 
-console.log(name1 + name2);
+// console.log(name1 + name2);
 
-//5. Add, subract, multiply and divide the strings '5' and '3'. Observe and explain the results.
+// //5. Add, subract, multiply and divide the strings '5' and '3'. Observe and explain the results.
+// console.log('5' + '3');
+//      Concatenates the two strings
+// console.log('5' - '3');
+//      Uses actual subtraction
+// console.log('5' * '3');
+//     Uses actual multiplication
+// console.log('5' / '3');
+//     Uses actual division
 
-//6. Create a prompt that asks a user to pick a number. Double the answer.
-var timesTwo = prompt("Pick a number, any number.");
-console.log(2 * timesTwo);
+// //6. Create a prompt that asks a user to pick a number. Double the answer.
+// var timesTwo = prompt("Pick a number, any number.");
+// 2 * timesTwo;
 
 //7. Create a custom alert.
+// alert("Hit OK if you're okay!");
 
 //8. Declare three variables called length, width and height. Calculate area and volume.
-var length = 10
-var height = 15
-var width = 20
-var area = length * width;
-var volume = length * width * height;
+// var length = 10
+// var height = 15
+// var width = 20
+// var area = length * width;
+// var volume = length * width * height;
 
-console.log(area);
-console.log(volume);
 
 //9. Ask for a user's name, age and city. In a single command display/log the sentence:
 // "My name is...I'm....years old, and I live in....."
+// var name = prompt("Enter your name.");
+// var age = prompt("Enter your age.");
+// var city = prompt("Enter your city.");
+
+// console.log("My name is " + name + ". I'm " + age + " years old, and I live in " + city + ".");
 
 //10. Create an alert that uses the response from a prompt.
+// var color = prompt("What is your favorite color?");
+// alert("My favorite color is " + color +" as well.  Let's be besties!");
